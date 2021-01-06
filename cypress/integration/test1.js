e6cbb9269fb4c85200fb2964cbc10029571c3a43
macbook <pavel.smoljaninov@yahoo.com>
2021-01-05 21:18:45 -0500

    First commit

---
+++ cypress/integration/test1.js
@@ -0,0 +1,12 @@
+/// <reference types=" Cypress" />
+it('google test', function (){
+    /*cy.visit')*/
+    cy.get('.gLFyf').type('Automation Step by Step{enter}')
+    cy.get('.dmenKe > .cIkxbf > .usJj9c > h3 > .l').click()
+
+
+})
+
+
+
+

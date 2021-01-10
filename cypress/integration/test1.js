1b86216909f5a57ebf3785541384e1c2c3603b02
pasok-a <pasok.a@yahoo.com>
2021-01-10 01:41:20 -0500

    update

--- cypress/integration/test1.js
+++ cypress/integration/test1.js
@@ -5,7 +5,7 @@ it.only('google test', function (){
     cy.xpath('//input[@title=\'Search\']').type('Automation Step by Step{enter}')
    /* cy.xpath('//h3[normalize-space()=\'Automation Step by ' +
         'Step: NevLearninger Stop Learning ...\']').click()*/
-    //cy.xpath('//input[@id=\'gbqfbb\']').click()
+    cy.xpath('//input[@id=\'gbqfbb\']').click()
 })
 
 

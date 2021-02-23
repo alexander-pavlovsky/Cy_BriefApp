4cddf1c912e1f4e3e375179af01aad69613879fb
pasok-a <pasok.a@yahoo.com>
2021-02-23 18:15:54 -0500

    update

--- cypress/integration/help.specs.js
+++ cypress/integration/help.specs.js
@@ -1,1 +1,18 @@
 /// <reference types="cypress" />
+describe("Contact", () => {
+
+    it("Verify help page ", function () {
+        cy.login();
+        cy.get('[title="Help"]').click();
+
+        // User should be redirected to Feedback & Support page after clicking on 'Help' link
+        cy.get('[class="drop-down-plain-list-cmp__item-text"]')
+            .contains("Support & Feedback")
+            .click();
+        cy.get('[class="usual-header-cmp__main-title"]').contains(
+            "Brief Feedback & Support "
+        );
+
+    });
+});
+

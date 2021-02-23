0095ff93064aa877676c40e117acc7265ec96bcc
pasok-a <pasok.a@yahoo.com>
2021-02-23 18:17:10 -0500

    update

--- cypress/integration/help.specs.js
+++ cypress/integration/help.specs.js
@@ -12,6 +12,20 @@ describe("Contact", () => {
         cy.get('[class="usual-header-cmp__main-title"]').contains(
             "Brief Feedback & Support "
         );
+// User should be able to use chat on the Feedback & Support page
+        cy.get(".public-DraftStyleDefault-block").type("Help");
+        cy.get(".chat-input-send-icon-cmp > svg > circle").click({ force: true });
+        cy.get(".event-text").contains("Help");
+
+        // User should be able to clear history on the Feedback & Support page
+        cy.get(
+            ".header-icon-with-popup > .base-header-icon-cmp > .icon-cmp"
+        ).click();
+        cy.contains("Clear History").click();
+        cy.get(".maat-long-button_warning").click();
+        cy.get(".event-text").should("not.exist");
+
+
 
     });
 });

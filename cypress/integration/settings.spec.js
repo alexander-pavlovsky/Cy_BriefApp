3bf628c75469963d8af95fc2c41233743449c516
pasok-a <pasok.a@yahoo.com>
2021-03-27 19:28:53 -0400

    update

--- cypress/integration/settings.spec.js
+++ cypress/integration/settings.spec.js
@@ -1,1 +1,24 @@
 /// <reference types="cypress" />
+
+
+describe("Settings page ", () => {
+
+
+    it("Verify settings page ", function () {
+        cy.login();
+        cy.get('[title="Settings"]').click();
+
+        // Verify that 'Your plan'  is displayed on the Settings page
+        cy.get('[class="tariff-plan-info-cmp__tariff-name"]').should("be.visible");
+
+        // Verify that 'Status' is displayed on the Settings page
+        cy.get('[class="tariff-plan-info-cmp__status-name"]').should("be.visible");
+
+
+
+
+
+
+
+    });
+});

a3fa3da14776caf731e5430d78bab28e6c0eb160
pasok-a <pasok.a@yahoo.com>
2021-04-03 19:30:36 -0400

    update

--- cypress/integration/settings.spec.js
+++ cypress/integration/settings.spec.js
@@ -14,7 +14,24 @@ describe("Settings page ", () => {
         // Verify that 'Status' is displayed on the Settings page
         cy.get('[class="tariff-plan-info-cmp__status-name"]').should("be.visible");
 
+        // Verify that user's name in displayed on the Settings page
+        cy.get('[class="user-real-name-cmp__fullname"]').should("be.visible");
 
+        // Verify that user's email in displayed on the Settings page
+        cy.get(
+            '[class="new-settings-line__text new-settings-line__text_selectable"]'
+        ).should("be.visible");
+
+        // Verify that the nickname is displayed on the Settings page
+        cy.get('[class="new-settings-line__text"]').should("be.visible");
+
+        // Verify that the profile link is displayed on the Settings page
+        cy.get(
+            '[class="new-settings-line__text new-settings-line__extra-text new-settings-line__extra-text_bold"]'
+        ).contains("Copy Link");
+
+        // Verify that self chat is present
+        cy.get('[class="new-settings-line__text"]').contains("Self Chat");
 
 
 

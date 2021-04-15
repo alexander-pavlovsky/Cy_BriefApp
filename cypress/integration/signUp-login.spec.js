4c5d98fd63ee82ae7edeb749a00bd19fa86f98ee
pasok-a <pasok.a@yahoo.com>
2021-04-15 19:33:19 -0400

    update

--- cypress/integration/signUp-login.spec.js
+++ cypress/integration/signUp-login.spec.js
@@ -1,2 +1,13 @@
 /// <reference types="cypress" />
 
+describe("Sign up - login page ", () => {
+
+
+    // login button should have href link
+    it("login button should have link", () => {
+        cy.visit("https://www.gobrief.com/");
+        cy.get(".button-4").should("have.attr", "href");
+
+
+    });
+});
